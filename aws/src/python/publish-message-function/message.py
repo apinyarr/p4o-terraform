9cc3fc86030ca70de276423062ed31afc64cf8e8
apinyarr <apinyarr@hotmail.com>
2021-07-10 19:23:36 +0700

    debug function

--- aws/src/python/publish-message-function/message.py
+++ aws/src/python/publish-message-function/message.py
@@ -3,10 +3,9 @@ import json
 
 def lambda_handler(event, context):
 
-    # sqs = boto3.resource('sqs')
+    sqs = boto3.resource('sqs')
 
-    sqs = boto3.client('sqs', region_name="ap-southeast-1")
-
-    queue = sqs.get_queue_by_name(QueueName='demo-queue')
+    # queue = sqs.get_queue_by_name(QueueName='demo-queue')
+    queue = sqs.Queue(url='https://sqs.ap-southeast-1.amazonaws.com/125065023022/demo-queue')
 
     response = queue.send_message(MessageBody=json.dumps(event))
