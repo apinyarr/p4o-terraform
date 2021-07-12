1d1698bcfb0a524c0f448fcf8b0f155d1e253bdf
Apinya <apinyarr@hotmail.com>
2021-07-12 22:36:42 +0700

    add debug path

--- aws/main.tf
+++ aws/main.tf
@@ -119,16 +119,3 @@ module "api_gateway" {
     Name = "http-apigateway"
   }
 }
-
-# In according to https://github.com/hashicorp/terraform-provider-aws/issues/13625
-resource "aws_lambda_permission" "this" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = module.lambda_function.lambda_function_name // add a reference to your function name here
-  principal     = "apigateway.amazonaws.com"
-
-  # The /*/*/* part allows invocation from any stage, method and resource path
-  # within API Gateway REST API. the last one indicates where to send requests to.
-  # see more detail https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html
-  source_arn = "arn:aws:execute-api:ap-southeast-1:*/*/*/*"
-}
---
+++ bugfix/main.tf
@@ -0,0 +1,12 @@
+# In according to https://github.com/hashicorp/terraform-provider-aws/issues/13625
+resource "aws_lambda_permission" "this" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "publish-messages-function" // add a reference to your function name here
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/*/* part allows invocation from any stage, method and resource path
+  # within API Gateway REST API. the last one indicates where to send requests to.
+  # see more detail https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html
+  source_arn = "arn:aws:execute-api:ap-southeast-1:[api-id]/*/*/*"
+}
