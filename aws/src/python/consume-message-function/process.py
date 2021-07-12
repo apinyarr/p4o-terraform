e4bc6e5467c8e931e3a068dfb52f7ac78967f4c8
Apinya <apinyarr@hotmail.com>
2021-07-13 04:41:31 +0700

    add more logic to function

--- aws/src/python/consume-message-function/process.py
+++ aws/src/python/consume-message-function/process.py
@@ -4,14 +4,14 @@ import json
 def lambda_handler(event, context):
     # Create SQS client
     sqs = boto3.client('sqs')
-    queue_url = 'https://sqs.ap-southeast-1.amazonaws.com/125065023022/demo-queue'
+    queue_url = 'https://sqs.ap-southeast-1.amazonaws.com/125065023022/demo-dlq'
     # Receive message from SQS queue
     response = sqs.receive_message(
         QueueUrl=queue_url,
         AttributeNames=[
             'string'
         ],
-        MaxNumberOfMessages=1,
+        MaxNumberOfMessages=10,
         MessageAttributeNames=[
             'All'
         ],
@@ -19,8 +19,8 @@ def lambda_handler(event, context):
         WaitTimeSeconds=5
     )
 
-    message = response['Messages'][0]
-    receipt_handle = message['ReceiptHandle']
+    # message = response['Messages'][0]
+    # receipt_handle = message['ReceiptHandle']
 
     # Delete received message from queue
     # sqs.delete_message(
--- aws/src/python/publish-message-function/message.py
+++ aws/src/python/publish-message-function/message.py
@@ -5,4 +5,26 @@ def lambda_handler(event, context):
     sqs = boto3.resource('sqs')
     queue = sqs.get_queue_by_name(QueueName='demo-queue')
     response = queue.send_message(MessageBody=json.dumps(event))
-    return json.dumps(event['rawPath'])
+    path = json.dumps(event['rawPath'])
+    if path.find("failure") != False:
+        # Force publish message to dlq
+        response = queue.send_message(MessageBody=json.dumps(event))
+        response = queue.send_message(MessageBody=json.dumps(event))
+        response = queue.send_message(MessageBody=json.dumps(event))
+        response = queue.send_message(MessageBody=json.dumps(event))
+        sqs2 = boto3.client('sqs')
+        queue_url = 'https://sqs.ap-southeast-1.amazonaws.com/125065023022/demo-queue'
+        # Receive message from SQS queue
+        response = sqs2.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=[
+                'string'
+            ],
+            MaxNumberOfMessages=10,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=5
+        )
+    return path
