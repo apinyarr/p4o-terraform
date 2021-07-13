42bcb407cad5ea6a50871bd923f4f93cc0ec5968
Apinya <apinyarr@hotmail.com>
2021-07-14 00:35:18 +0700

    correct function logic

--- aws/src/python/consume-message-function/process.py
+++ aws/src/python/consume-message-function/process.py
@@ -2,25 +2,28 @@ import boto3
 import json
 
 def lambda_handler(event, context):
-    # Create SQS client
-    sqs = boto3.client('sqs')
-    queue_url = 'https://sqs.ap-southeast-1.amazonaws.com/125065023022/demo-dlq'
-    # Receive message from SQS queue
-    response = sqs.receive_message(
-        QueueUrl=queue_url,
-        AttributeNames=[
-            'string'
-        ],
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=[
-            'All'
-        ],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=0
-    )
+    for record in event['Records']:
+        print("test")
+        payload = record["body"]
+        print(str(payload))
+    # # Create SQS client
+    # sqs = boto3.client('sqs')
+    # queue_url = 'https://sqs.ap-southeast-1.amazonaws.com/125065023022/demo-dlq'
+    # # Receive message from SQS queue
+    # response = sqs.receive_message(
+    #     QueueUrl=queue_url,
+    #     AttributeNames=[
+    #         'string'
+    #     ],
+    #     MaxNumberOfMessages=1,
+    #     MessageAttributeNames=[
+    #         'All'
+    #     ],
+    #     VisibilityTimeout=0,
+    #     WaitTimeSeconds=0
+    # )
 
-    message = response['Messages'][0]
-    return json.dumps(message)
+    # message = response['Messages'][0]
     # receipt_handle = message['ReceiptHandle']
 
     # Delete received message from queue
