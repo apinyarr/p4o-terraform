4f3e72452fde8e8880d4bec0facd826cdf4c09c1
Apinya <apinyarr@hotmail.com>
2021-07-14 14:06:58 +0700

    modify function logic

--- aws/src/python/consume-message-function/process.py
+++ aws/src/python/consume-message-function/process.py
@@ -13,23 +13,34 @@ example in the Amazon Kinesis Data Analytics SQL Developer Guide.
 import json
 import boto3
 
-STREAM_NAME = "terraform-kinesis-test"
+# STREAM_NAME = "terraform-kinesis-test"
+# kinesis_client = boto3.client('kinesis')
 
+# def get_data():
+#     return {'log': '192.168.254.30 - John [24/May/2004:22:01:02 -0700] '
+#                    '"GET /icons/apache_pb.gif HTTP/1.1" 304 0'}
 
-def get_data():
-    return {'log': '192.168.254.30 - John [24/May/2004:22:01:02 -0700] '
-                   '"GET /icons/apache_pb.gif HTTP/1.1" 304 0'}
+# def generate(stream_name, kinesis_client):
+#     while True:
+#         data = get_data()
+#         print(data)
+#         kinesis_client.put_record(
+#             StreamName=stream_name,
+#             Data=json.dumps(data),
+#             PartitionKey="partitionkey")
 
+# if __name__ == '__main__':
+#     generate(STREAM_NAME, boto3.client('kinesis'))
 
-def generate(stream_name, kinesis_client):
-    while True:
-        data = get_data()
-        print(data)
+def lambda_handler(event, context):
+    STREAM_NAME = "terraform-kinesis-test"
+    kinesis_client = boto3.client('kinesis')
+    for record in event['Records']:
+        print("new record")
+        payload = record["body"]
+        print(str(payload))
         kinesis_client.put_record(
-            StreamName=stream_name,
-            Data=json.dumps(data),
-            PartitionKey="partitionkey")
-
-
-if __name__ == '__main__':
-    generate(STREAM_NAME, boto3.client('kinesis'))
+            StreamName=STREAM_NAME,
+            Data=json.dumps(payload),
+            PartitionKey="partitionkey"
+        )
